3904c7f368aadd030175f153c6a8f730d9abe096
dan929873 <metallica04@yandex.ru>
2021-11-16 22:48:49 +0300

    less2

--- geekshop/geekshop/settings.py
+++ geekshop/geekshop/settings.py
@@ -53,7 +53,7 @@ ROOT_URLCONF = 'geekshop.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [BASE_DIR / 'template']
+        'DIRS': [BASE_DIR / 'templates']
         ,
         'APP_DIRS': True,
         'OPTIONS': {
