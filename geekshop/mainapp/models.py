7d171c7f574d036bc08891f2e631656624a01662
dan929873 <metallica04@yandex.ru>
2021-11-20 12:02:11 +0300

    less3 need add in view DB

--- geekshop/mainapp/models.py
+++ geekshop/mainapp/models.py
@@ -2,6 +2,39 @@ from django.db import models
 
 # Create your models here.
 class ProductCategory(models.Model):
-    name = models.CharField(max_length=64)
+    name = models.CharField(max_length=128)
     description = models.TextField(blank=True, null=True)
-    # 1.03
+
+    def __str__(self):
+        return f'{self.name}'
+
+class Product(models.Model):
+    name = models.CharField(max_length=128)
+    image = models.ImageField(upload_to='product_image', blank=True)
+    description = models.TextField(blank=True, null=True)
+    price = models.DecimalField(max_digits=8, decimal_places=2)
+    quantity = models.PositiveIntegerField(default=0)
+    category = models.ForeignKey(ProductCategory, on_delete=models.CASCADE)
+
+    def __str__(self):
+        return f'{self.name} | {self.category}'
+
+# >>> from mainapp.models import ProductCategory
+# >>> category = ProductCategory(name= 'Одежда', description = 'Описание одежды')
+# >>> category.save()
+# >>> category_1 = ProductCategory.objects.create(name='Аксессуары', description ='')
+# >>> category_2 = ProductCategory.objects.create(name='Обувь', description ='')
+# >>> a = ProductCategory.objects.get(name='Одежда')
+# >>> print(a)
+# Одежда
+# >>> print(a.description)
+# Описание одежды
+# >>> a = ProductCategory.objects.all()
+# >>> for i in a:
+# ...     print(i)
+# ...
+# Одежда
+# Аксессуары
+# Обувь
+# >>>
+
